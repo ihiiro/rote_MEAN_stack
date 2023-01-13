22bfc927a5af106a62e544e0f931f4c3619199fc
hiro <yassirelyakine@gmail.com>
2023-01-13 13:28:58 +0100

    the http module

--- node_express_mongodb/app.js
+++ node_express_mongodb/app.js
@@ -1,30 +1,14 @@
-const fs = require('fs');
+const http = require('http');
 
-/* synchronous */
-
-// reading
-console.log(fs.readFileSync('../cli.txt', 'utf8'));
-console.log(fs.readFileSync('../notes.txt', 'utf8'));
-
-// writing
-// flag: 'a' is for appending
-fs.writeFileSync('../new_file.txt', 'there ya go!'/*, { flag: 'a' }*/);
-
-
-/* asynchronous */
-
-fs.readFile('../new_file.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // shorthand for res.write(...) => res.end()
+    res.end("<h1>Homepage</h1>");
+  } else {
+    // message to show when someone connects to server
+    res.end("<h1>Page not found</h1>");
   }
 
-  console.log(result);
 });
 
-fs.writeFile('../new_file1.txt', 'another file, howdy!', /*{flag:'a'},*/ (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-});
+server.listen(8080);
