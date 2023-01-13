b8de43cb25aa67fe7f3b5e4742ff51612ae8963a
hiro <yassirelyakine@gmail.com>
2023-01-13 15:11:05 +0100

    add nodemon package for change detection

--- node_express_mongodb/app.js
+++ node_express_mongodb/app.js
@@ -1,14 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    // shorthand for res.write(...) => res.end()
-    res.end("<h1>Homepage</h1>");
-  } else {
-    // message to show when someone connects to server
-    res.end("<h1>Page not found</h1>");
-  }
-
-});
-
-server.listen(8080);
+console.log("hello");
