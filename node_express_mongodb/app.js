8507c8c11989144e4d877c16a1a4d026b719b306
hiro <yassirelyakine@gmail.com>
2023-01-13 22:43:08 +0100

    events in node

--- node_express_mongodb/app.js
+++ node_express_mongodb/app.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const event_emitter = require('events');
+
+const custom_emitter = new event_emitter();
+
+// setup event output
+custom_emitter.on('response', () => {
+  console.log("Event \"response\" has been emitted");
+});
+
+// multiple callbacks for the same event
+custom_emitter.on('response', () => {
+  console.log("Event \"response\" has been EMITTED");
+});
+
+// callback with parameters
+custom_emitter.on('response', (username) => {
+  console.log(`Hi ${username}!`);
+});
+
+// trigger event
+custom_emitter.emit('response', "Yassir");
