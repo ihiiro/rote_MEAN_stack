1c1e5b600818db964512ccba08b7541e2a5309f1
hiro <yassirelyakine@gmail.com>
2023-01-13 01:00:56 +0100

    some basic OS module usage examples

--- node_express_mongodb/app.js
+++ node_express_mongodb/app.js
@@ -1,2 +1,20 @@
-// this will execute code from 'other_file.js'
-require("./other_file");
+const os = require('os');
+
+console.log(os.userInfo());
+
+console.log("\n");
+
+// number of seconds the computer has been running
+// the expression gets time in hours
+console.log(`The computer has been running for ~${Math.round(os.uptime()/3600)}hours.`);
+
+console.log("\n");
+
+const os_info = {
+  type: os.type(),
+  release: os.release(),
+  totalmem: `~${Math.round(os.totalmem()/1073741824)}GB`,
+  freemem: `~${Math.round(os.freemem()/1073741824)}GB`,
+};
+
+console.log(os_info);
