f6ec0888d68df4d8ec9feae40fc64c7f95725c16
hiro <yassirelyakine@gmail.com>
2023-01-14 22:23:49 +0100

    expressJS middleware

--- node_express_mongodb/app.js
+++ node_express_mongodb/app.js
@@ -1,62 +1,28 @@
 const express = require('express');
-const os = require('os');
-const path = require('path');
-
-const users = require('./users.js');
 
 const app = express();
 
-// provide static files directory
-app.use(express.static('./static'));
+// req => middleware(logger) => res
+// it's best practice to put middleware in a separate file
+const logger = (req, res, next) => {
+  console.log(req.method, req.url, new Date());
+  next();
+};
 
-app.get('/', (req, res) => {
-  res.status(200).send(`SERVER UPTIME: ~${Math.round(os.uptime()/3600)} hours <br><br> SERVER TYPE: ${os.type()}`);
+app.use(logger);
+
+// for multiple middleware functions
+// execution order: FIFO(first in first out) so the order in the array matters
+// app.use([mw0, mw1, mwN])
+
+// method one: pass middleware explicitly
+app.get('/', logger, (req, res) => {
+  res.status(200).send('Home');
 });
 
-app.get('/uptime', (req, res) => {
-  res.status(200).send(`SERVER UPTIME: ~${Math.round(os.uptime()/3600)} hours`);
-});
-
-app.get('/type', (req, res) => {
-  res.status(200).send(`SERVER TYPE: ${os.type()}`);
-});
-
-// send file
-app.get('/htmlfile', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, './html_file.html'));
-});
-
-// send json
-app.get('/apis/json', (req, res) => {
-  res.status(200).json([{name:'yassir'}, {age: 20}]);
-});
-app.get('/apis/users', (req, res) => {
-  res.status(200).json(users);
-});
-
-// url parameters
-// :parameter
-app.get('/greet:name', (req, res) => {
-  res.send(`Greetings ${req.params.name}!`);
-});
-
-// url queries
-// example query: /apis/users/user?username=yassir&age=20
-app.get('/apis/users/user', (req, res) => {
-  const query_username = req.query.username;
-  const query_age = req.query.age;
-
-  const user_object = users.filter((user) => {
-    return user.username === query_username && user.age === Number(query_age);
-  });
-
-  res.status(200).json(user_object)
-});
-
-
-// error page for when accessing invalid resources
-app.all('*', (req, res) => {
-  res.status(404).send('invalid url.');
+// method two: doing nothing cuz app.use() was used (no pun intended)
+app.get('/about', (req, res) => {
+  res.status(200).send('About');
 });
 
 app.listen(8080, () => {
